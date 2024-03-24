b522292e0b3af6303756e6593c0f6312edc64b36
SoheilKhorram <soheil13261428@gmail.com>
2024-03-24 15:58:28 +0430

    adding font to the project

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
+import SignupScreen from './signup/index.jsx'
+import { Link } from "expo-router"
+import { useFonts } from 'expo-font'
+
 const index = () => {
+    const [fontLoaded] = useFonts({
+        'YekanBold': require('../assets/fonts/YekanBold.ttf'),
+        'Yekan': require('../assets/fonts/Yekan.ttf'),
+    })
+
+    if (!fontLoaded) {
+        return <Text>Loading...</Text>
+    }
+
     return (
         <View>
+            <Link href='signup'>signup</Link>
             <Text>index</Text>
-        </View>
+        </View >
     )
 }
 export default index
+
 const styles = StyleSheet.create({})
